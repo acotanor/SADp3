fe615a902f169966f1294c6171bd5a12d874167c
acotanor <acotanor@gmail.com>
2025-03-03 12:02:59 +0100

    Corrección: devuelve un diccionario vacío si hay un error.

--- leerJSON.py
+++ leerJSON.py
@@ -3,11 +3,16 @@ import sys
 
 # Dado un archivo JSON devuelve un diccionario con el formato:
 # atributo : valor
-def leerJson(file):
+def leerJson(file) -> dict:
     try:
-        with open(file,"r") as jsonFile:    # Abre el archivo.
-            return json.load(jsonFile)      # Carga los datos en un diccionario.
+        with open(file,"r") as jsonFile:        # Abre el archivo.
+            return json.load(jsonFile)          # Carga los datos en un diccionario.
     except FileNotFoundError:
-        return Null
+        print(f"El archivo {file} no existe.")
+        return {}                               # Diccionario vacío.
     except json.JSONDecodeError:
-        return Null
+        print("Formato incorrecto.")
+        return {}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
