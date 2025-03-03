16e6cac65f9afeda077212e79ad53af91d37551e
acotanor <acotanor@gmail.com>
2025-03-03 12:02:02 +0100

    Gestión de errores.

--- modJSON.py
+++ modJSON.py
@@ -4,10 +4,15 @@ import leerJSON
 
 # Dado un archivo JSON, el atributo a cambiar y el valor modificado, modifica los metadatos.
 def modJson(file, atributo, valor):
-    data = leerJSON.leerJson(file)              # Lee el archivo JSON a modificar.
-    data["preprocessing"][atributo] = valor     # Cambiamos el valor de un atributo.
+    try:
+        data = leerJSON.leerJson(file)              # Lee el archivo JSON a modificar.
+        data["preprocessing"][atributo] = valor     # Cambiamos el valor de un atributo.
 
-    with open(file, "w") as jsonFile:        
-        json.dump(data, jsonFile, indent=4)     # Aplicamos los cambios en el archivo.
-        
-    print(leerJSON.leerJson(file))
+        with open(file, "w") as jsonFile:        
+            json.dump(data, jsonFile, indent=4)     # Aplicamos los cambios en el archivo.
+    except FileNotFoundError:
+        print(f"El archivo {file} no existe.")
+    except json.JSONDecodeError:
+        print("Formato incorrecto.")
+    except Exception as e:
+        print(f"Error: {e}")
