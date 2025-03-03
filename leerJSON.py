5f566e4e3ae289f2ad3e690123f0028e4ddf5e5d
acotanor <acotanor@gmail.com>
2025-03-03 11:24:31 +0100

    Gestión de errores.

--- leerJSON.py
+++ leerJSON.py
@@ -1,10 +1,13 @@
 import json
 import sys
 
+# Dado un archivo JSON devuelve un diccionario con el formato:
+# atributo : valor
 def leerJson(file):
-    with open(file,"r") as jsonFile:
-        data = json.load(jsonFile)
-    print(data)
-
-
-leerJson("ej.json")
+    try:
+        with open(file,"r") as jsonFile:    # Abre el archivo.
+            return json.load(jsonFile)      # Carga los datos en un diccionario.
+    except FileNotFoundError:
+        return Null
+    except json.JSONDecodeError:
+        return Null
