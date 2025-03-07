8ccc6a22d7975042dd6d94e241b2189e2ef3764f
plopez053 <peiolpz02@gmail.com>
2025-03-07 10:32:02 +0100

    jjj

--- plantilla.py
+++ plantilla.py
@@ -5,6 +5,32 @@ def load_data(file):
     # Carga de datos
     data = pandas.read_csv(file)
     return data
+def select_features():
+    """
+    Separa las características del conjunto de datos en características numéricas, de texto y categóricas.
+
+    Returns:
+        numerical_feature (DataFrame): DataFrame que contiene las características numéricas.
+        text_feature (DataFrame): DataFrame que contiene las características de texto.
+        categorical_feature (DataFrame): DataFrame que contiene las características categóricas.
+    """
+    try:
+        # Numerical features
+        numerical_feature = data.select_dtypes(include=['int64', 'float64']) # Columnas numéricas
+        if args.prediction in numerical_feature.columns:
+            numerical_feature = numerical_feature.drop(columns=[args.prediction])
+        # Categorical features
+        categorical_feature = data.select_dtypes(include='object')
+        categorical_feature = categorical_feature.loc[:, categorical_feature.nunique() <= args.preprocessing["unique_category_threshold"]]
+        
+        # Text features
+        text_feature = data.select_dtypes(include='object').drop(columns=categorical_feature.columns)
+        
+    except Exception as e:
+        print(Fore.RED+"Error al separar los datos"+Fore.RESET)
+        print(e)
+        sys.exit(1)
+
 if __name__ == '__main__':
     # Carga de datos
     file_path = input("Introduce la ruta del archivo: ")
